14e32249b59cb53977b747624b372f1a378a1a5c
interdigitize <interdigitize@users.noreply.github.com>
2017-05-14 16:06:23 -0700

    adds the stringify function that passes ALL tests

--- jsoninator.js
+++ jsoninator.js
@@ -4,10 +4,21 @@ const _ = require('underscore'); // the real one! :)
 // const stringify = JSON.stringify;
 // But you don't. So you're going to write it from scratch...
 
-
-
 const stringify = function(obj) {
-  // your code goes here
+
+  if(typeof obj === 'number' || typeof obj === 'boolean'){
+    return obj.toString();
+  } else if (obj === null){
+    return 'null';
+  } else if (typeof obj  === 'string'){
+    return `"${obj}"`;
+  } else if (Array.isArray(obj)){
+    return `[${_.map(obj, el => stringify(el))}]`
+  }
+  else {
+    let string = _.map(obj, (el, key) => `\"${key}\":${stringify(el)}`);
+    return `{${string}}`;
+  }
 };
 
 module.exports = {
