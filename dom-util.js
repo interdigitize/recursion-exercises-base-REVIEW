bf24268a933211caf49693a1e086dc6ea02a48f4
interdigitize <interdigitize@users.noreply.github.com>
2017-05-14 14:32:26 -0700

    adds visitAllNodes and flattenTreeToArray functions that pass tests

--- dom-util.js
+++ dom-util.js
@@ -1,12 +1,14 @@
 
 const visitAllNodes = function(node, callback) {
-  // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
-  // Your code here
+  let children = node.childNodes;
+  callback(node);
+  return children.forEach(childNode => visitAllNodes(childNode, callback));
 };
 
 const flattenTreeToArray = function(node) {
-  // Hint: Use visitAllNodes()
-  // Your code here
+  let result = [];
+  visitAllNodes(node, childNode => result.push(childNode));
+  return result;
 };
 
 module.exports = {
