cd20229292a7ae51eed3596cdbad16f6689e9081
interdigitize <interdigitize@users.noreply.github.com>
2017-05-30 11:31:12 -0700

    update visitAllNodes function

--- dom-util.js
+++ dom-util.js
@@ -1,7 +1,18 @@
 
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
-  // Your code here
+
+  // set a variable equal to the children elements and convert the node list to an array
+  children = Array.from(node.childNodes);
+
+  // call the callback on the initial node;
+  callback(node);
+
+  // iterate through the array and call the callback on each child
+  return children.forEach(function(node){
+    callback(node);
+  });
+
 };
 
 const flattenTreeToArray = function(node) {
@@ -13,3 +24,33 @@ module.exports = {
   visitAllNodes: visitAllNodes,
   flattenTreeToArray: flattenTreeToArray
 };
+
+
+// var test = [
+//   something: {
+//     name: 'thing',
+//     color: 'neutral',
+//     type: 'generic',
+//     numbers: [1, 5, 3],
+//     aChild: {
+//       name: 'thing',
+//       color: 'neutral',
+//       type: 'generic',
+//       expression: 'cool'
+//     }
+//   },
+//   another: {
+//     name: 'anada',
+//     color: 'blue',
+//     type: 'interesting',
+//     numbers: [2, 3],
+//     aChild: {
+//       name: 'twoBit',
+//       color: 'seafoam',
+//       type: 'interesting',
+//       expression: 'sick'
+//     }
+//   }
+// ];
+//
+// visitAllNodes()
